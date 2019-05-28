460706912f34f361733d62eefecd600d39d321a7
kirbywithaknife <50634824+kirbywithaknife@users.noreply.github.com>
2019-05-28 14:08:11 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,73 @@
+prophecies_url = "http://sf-pyw.mosyag.in/m04/api/forecasts"
+
+$("#header").click(function() {
+  
+  $.getJSON(
+    prophecies_url, 
+    function(data) {
+      advice = data["prophecies"],
+      set_content_in_divs(advice)
+    }, 
+    console.log("True"),
+  );
+  $(document).ready(function(){
+      $("#p-0").hover(
+        function() {
+          $(this).css("background-color", "#ccffff");
+        },
+        function(){
+          $(this).css("background-color", "white")
+        }
+      );
+      $("#p-1").hover(
+        function() {
+          $(this).css("background-color", "#ccffff");
+        },
+        function(){
+          $(this).css("background-color", "white")
+        }
+      );
+      $("#p-2").hover(
+        function() {
+          $(this).css("background-color", "#ccffff");
+        },
+        function(){
+          $(this).css("background-color", "white")
+        }
+      );
+      $("#p-3").hover(
+        function() {
+          $(this).css("background-color", "#ccffff");
+        },
+        function(){
+          $(this).css("background-color", "white")
+        }
+      );
+      $("#p-4").hover(
+        function() {
+          $(this).css("background-color", "#ccffff");
+        },
+        function(){
+          $(this).css("background-color", "white")
+        }
+      );
+      $("#p-5").hover(
+        function() {
+          $(this).css("background-color", "#ccffff");
+        },
+        function(){
+          $(this).css("background-color", "white")
+        }
+      );
+  });
+})
+
+function set_content_in_divs(prophecies) {
+    $.each(
+      prophecies, 
+      function(i, d) {
+        p = $("#p-" + i)
+        p.html("<p>" + d + "</p>")
+      }
+    ), console.log("True")
+}
